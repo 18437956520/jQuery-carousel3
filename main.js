5676fd2297a0670eccd6e832d4d4078c2b13a565
yyyh <444510796@qq.com>
2019-04-09 21:57:16 +0800

    ..

--- main.js
+++ main.js
@@ -14,50 +14,62 @@ bindEvents()
 //封装优化代码
 
 function bindEvents(){
-    $buttons.eq(0).on('click', function(){
-        if(current == 5){
-            $slides.css({transform:'translateX(-2100px)'})
-                .one('transitionend', function(){
-                    $slides.hide()
-                        .offset()
-                    $slides.css({transform:'translateX(-300px)'})
-                        .show()
-                })    
-        }else{
-            $slides.css({transform:'translateX(-300px)'})    
-        }
-        current = 0
+    $('#button').on('click', 'button', function(x){
+        let $button = $(x.currentTarget)
+        let index = $button.index()
+        
     })
-    $buttons.eq(1).on('click', function(){
-        $slides.css({transform:'translateX(-600px)'})
-        current = 1
-    })
-    $buttons.eq(2).on('click', function(){
-        $slides.css({transform:'translateX(-900px)'})
-        current = 2
-    })
-    $buttons.eq(3).on('click', function(){
-        $slides.css({transform:'translateX(-1200px)'})
-        current = 3
-    })
-    $buttons.eq(4).on('click', function(){
-        $slides.css({transform:'translateX(-1500px)'})
-        current = 4
-    })
-    $buttons.eq(5).on('click', function(){
-        if(current == 0){
-            $slides.css({transform:'translateX(0px)'})
-                .one('transitionend', function(){
-                    $slides.hide()
-                        .offset()
-                    $slides.css({transform:'translateX(-1800px)'})
-                        .show()
-            })
-        }else{
-            $slides.css({transform:'translateX(-1800px)'})
-        }
-        current = 5
-    })
+
+
+
+
+
+
+
+    // $buttons.eq(0).on('click', function(){
+    //     if(current == 5){
+    //         $slides.css({transform:'translateX(-2100px)'})
+    //             .one('transitionend', function(){
+    //                 $slides.hide()
+    //                     .offset()
+    //                 $slides.css({transform:'translateX(-300px)'})
+    //                     .show()
+    //             })    
+    //     }else{
+    //         $slides.css({transform:'translateX(-300px)'})    
+    //     }
+    //     current = 0
+    // })
+    // $buttons.eq(1).on('click', function(){
+    //     $slides.css({transform:'translateX(-600px)'})
+    //     current = 1
+    // })
+    // $buttons.eq(2).on('click', function(){
+    //     $slides.css({transform:'translateX(-900px)'})
+    //     current = 2
+    // })
+    // $buttons.eq(3).on('click', function(){
+    //     $slides.css({transform:'translateX(-1200px)'})
+    //     current = 3
+    // })
+    // $buttons.eq(4).on('click', function(){
+    //     $slides.css({transform:'translateX(-1500px)'})
+    //     current = 4
+    // })
+    // $buttons.eq(5).on('click', function(){
+    //     if(current == 0){
+    //         $slides.css({transform:'translateX(0px)'})
+    //             .one('transitionend', function(){
+    //                 $slides.hide()
+    //                     .offset()
+    //                 $slides.css({transform:'translateX(-1800px)'})
+    //                     .show()
+    //         })
+    //     }else{
+    //         $slides.css({transform:'translateX(-1800px)'})
+    //     }
+    //     current = 5
+    // })
 }
 
 function makeFakeSlides(){
