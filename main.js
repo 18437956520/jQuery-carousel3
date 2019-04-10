936891c873848c5d70ffe71f4e44897d05bb9590
yyyh <444510796@qq.com>
2019-04-10 10:55:22 +0800

    优化

--- main.js
+++ main.js
@@ -17,30 +17,31 @@ function bindEvents(){
     $('#button').on('click', 'button', function(x){
         let $button = $(x.currentTarget)
         let index = $button.index()
-
-        if(current === $buttons.length-1 && index === 0){
-            //最后一张到第一张
-            $slides.css({transform:`translateX(${-($buttons.length + 1)*300}px)`})
-                .one('transitionend', function(){
-                    $slides.hide().offset()
-                    $slides.css({transform:`translateX(${-(index+1)*300}px)`}).show()
-                })
-
-        }else if(current === 0 && index === $buttons.length-1){
-            $slides.css({transform:`translateX(0px)`})
-                .one('transitionend', function(){
-                    $slides.hide().offset()
-                    $slides,css({transform:`translateX(${-(index+1)*300}px)`}).show()
-                })
-
-        }else{
-            $slides.css({transform:`translateX(${-(index+1)*300}px)`})
-        }
-        current = index
-
+        goToSlide(index)
     })
+}
 
+function goToSlide(index){
+    if(current === $buttons.length-1 && index === 0){
+        //最后一张到第一张
+        $slides.css({transform:`translateX(${-($buttons.length + 1)*300}px)`})
+            .one('transitionend', function(){
+                $slides.hide().offset()
+                $slides.css({transform:`translateX(${-(index+1)*300}px)`}).show()
+            })
 
+    }else if(current === 0 && index === $buttons.length-1){
+        $slides.css({transform:`translateX(0px)`})
+            .one('transitionend', function(){
+                $slides.hide().offset()
+                $slides,css({transform:`translateX(${-(index+1)*300}px)`}).show()
+            })
+
+    }else{
+        $slides.css({transform:`translateX(${-(index+1)*300}px)`})
+    }
+    current = index
+}
 
 function makeFakeSlides(){
     let $firstCopy = $images.eq(0).clone(true)
